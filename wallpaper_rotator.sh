f213e52f929ef5cfa938d50a6f6b18a8ad648da6
Ricard Santiago Raigada García <ricardraigada@outlook.es>
2025-04-24 12:38:43 +0200

    fix: Use dconf to set picture-uri-dark and ensure background changes in GNOME dark mode.

--- wallpaper_rotator.sh
+++ wallpaper_rotator.sh
@@ -6,7 +6,7 @@
 #       .webp images from 'walls' subfolders within the main folder.
 # ------------------------------------------------------------------------------
 
-WALL_DIR="Enter your wallpaper directory here" # Replace with your wallpaper directory
+WALL_DIR="/home/ricardraigada/Imágenes/Wallpapers" # Replace with your wallpaper directory
 if [ ! -d "$WALL_DIR" ]; then
     echo "The $WALL_DIR directory does not exist."
     exit 1
@@ -15,11 +15,13 @@ fi
 mapfile -t WALLPAPERS < <(find "$WALL_DIR" -type d -name "walls" -exec find {} -type f -name "*.webp" \;)
 
 if [ ${#WALLPAPERS[@]} -eq 0 ]; then
-    echo "No se encontraron wallpapers."
+    echo "No wallpapers found."
     exit 1
 fi
 
 RANDOM_WALL=$(shuf -n 1 -e "${WALLPAPERS[@]}")
+dconf write /org/gnome/desktop/background/picture-uri-dark "'file://$RANDOM_WALL'"
+gsettings set org.gnome.desktop.background picture-uri "file://$RANDOM_WALL" 2>/dev/null || 
 
-gsettings set org.gnome.desktop.background picture-uri "file://$RANDOM_WALL"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$RANDOM_WALL"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$RANDOM_WALL" 2>/dev/null || 
+echo "Fondo cambiado a: $RANDOM_WALL"
